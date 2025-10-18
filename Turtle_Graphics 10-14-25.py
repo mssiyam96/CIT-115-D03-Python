a2b8e3dfe025bacf23b8768b6a2b462645e0d84d
Mohi Uddin Siyam <63117523+mssiyam96@users.noreply.github.com>
2025-10-18 19:30:43 -0400

    Add files via upload

---
+++ Turtle_Graphics 10-14-25.py
@@ -0,0 +1,67 @@
+#Hit the target game
+import turtle
+
+#Name constants
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+TARGET_LLEFT_X = 100
+TARGET_LLEFT_Y = 250
+TARGET_WIDTH = 25
+FORCE_FACTOR = 30
+PROJECTILE_SPEED = 1
+NORTH = 90
+SOUTH = 270
+EAST = 0
+WEST = 180
+
+#SETUP WINDOW
+
+turtle.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+#draw target
+
+turtle.hideturtle()
+turtle.speed(0)
+turtle.penup()
+turtle.goto(TARGET_LLEFT_X, TARGET_LLEFT_Y)
+turtle.pendown()
+turtle.setheading(EAST)
+turtle.forward(TARGET_WIDTH)
+turtle.setheading(NORTH)
+turtle.forward(TARGET_WIDTH)
+turtle.setheading(WEST)
+turtle.forward(TARGET_WIDTH)
+turtle.setheading(SOUTH)
+turtle.forward(TARGET_WIDTH)
+turtle.penup()
+
+#center the turtle
+turtle.goto(0,0)
+turtle.setheading(EAST)
+turtle.showturtle()
+turtle.speed(PROJECTILE_SPEED)
+
+#get the angle
+
+angle = float(input('enter angle: '))
+force = float(input('enter force(1-10) '))
+
+distance = force * FORCE_FACTOR
+turtle.setheading(angle)
+turtle.pendown()
+turtle.forward(distance)
+
+if ( turtle.xcor() >= TARGET_LLEFT_X and
+    turtle.xcor() <= (TARGET_LLEFT_X + TARGET_WIDTH) and
+    turtle.ycor() >= TARGET_LLEFT_Y and
+    turtle.ycor() <= (TARGET_LLEFT_Y + TARGET_WIDTH)):
+
+    print('target hit')
+
+else:
+    print('you missed')
+    
+
+
+
+
